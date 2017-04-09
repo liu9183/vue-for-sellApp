090ad9ec23b673e6f949fda2b14c8fa015c29e28
liu9183 <710930665@qq.com>
2017-04-09 12:21:37 +0800

    sellApp

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <div class="header">
+    		   I am header
+    	</div>
+    	<div class="tab">
+    		   I am tab
+    	</div>
+    	<div class="content">
+    		   I am content
+    	</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+  
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './App';
+var v=new Vue({
+	 el:'body',
+	 companents:{App}
+});
