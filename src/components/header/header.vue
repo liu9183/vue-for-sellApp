b3e1df7de95b5def3a313924d467c3e798be6e36
liu9183 <710930665@qq.com>
2017-04-11 17:04:32 +0800

    sellapp

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,13 +13,25 @@
 					{{seller.description}}/{{seller.deliveryTime}}分钟送达
 				</div>
 				<div v-if="seller.supports" class="support">
-					<span class="icon"></span>
+					<span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div v-if="seller.supports" class="support-count">
+				<span class="count">
+					{{seller.supports.length}}个
+				</span>
+				<i class="icon-keyboard_arrow_right"></i>
+			</div>
 		</div>
 		<div class="bulletin-wrapper">
-			
+			<span class="bulletin-title">
+				
+			</span>
+			<span class="bulletin-text">
+				{{seller.bulletin}}
+			</span>
+			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 	</div>
 </template>
@@ -30,18 +42,23 @@
 	   	seller:{
 	   		type:Object
 	   	}
+	   },
+	   created(){
+	   	  this.classMap=['decrease','discount','guarantee','invoice','special']
 	   }
-	}
+	};
 </script>
 
 <style>
+	@import url("../../common/stylus/style.css");
 	.header{
-		background: #000;
+		background: #999;
 		color: #fff;
 	}
 	.header>.content_wrapper{
 		
 		padding: 24px 12px 18px 24px;
+		position: relative;
 	}
 	.header>.content_wrapper>.avatar{
 		
@@ -82,25 +99,59 @@
 	}
 	.content>.support>.icon{
 		display: inline-block;
+		vertical-align: bottom;
 		width: 12px;
 		height: 12px;
 		margin-right: 4px;
 		background-size: 12px 12px;
 		background-repeat: no-repeat;
 	}
-	.icon>.decrease{
+	.icon.decrease{
 		background-image: url(decrease_1@2x.png);
 	}
-	.icon>.discount{
+	.icon.discount{
 		background-image: url(discount_1@2x.png);
 	}
-	.icon>.guarantee{
+	.icon.guarantee{
 		background-image: url(guarantee_1@2x.png);
 	}
-	.icon>.invoice{
+	.icon.invoice{
 		background-image: url(invoice_1@2x.png);
 	}
-	.icon>.special{
+	.icon.special{
 		background-image: url(special_1@2x.png);
+	}
+	.content>.support>.text{
+		line-height: 12px;
+		font-size: 12px;
+		padding: 0;
+	}
+	.header>.content_wrapper>.support-count{
+		position: absolute;
+		right: 12px;
+		bottom: 14px;
+		padding: 0 8px;
+		height: 24px;
+		line-height: 20px;
+		border-radius: 14px;
+		background: rgba(0,0,0,0.2);
+		text-align: center;
+		
+	}
+	.support-count>.count{
+		font-size: 10px;
+	}
+	.support-count>.icon-keyboard_arrow_right{
+		font-size: 10px;
+		line-height: 24px;
+		margin-left: 2px;
+	}
+	.header>.bulletin-wrapper{
+		height: 28px;
+		line-height: 28px;
+		padding: 0 22px 0 12px;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 </style>
