fe2dae42770a09812cf6cb505a6a490f6a7c20d1
liu9183 <710930665@qq.com>
2017-04-14 20:54:25 +0800

    main-view

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,154 @@
 <template>
-	<div>
-		nihaoa
+	<div class="goods">
+		<div class="menu-wrapper">
+            <ul>
+            	<li v-for="item in goods" class="menu-item">
+            		<span class="text">
+            			<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+            			
+            		</span>
+            	</li>
+            </ul>
+		</div>
+		<div class="foods-wrapper">
+            <ul>
+            	<li v-for="item in goods" class="food-list">
+            		<h1 class="title">{{item.name}}</h1>
+            		<ul>
+            			<li v-for="food in item.foods" class="food-item">
+            				<div class="icon">
+            					<img :src="food.icon"/>
+            				</div>
+            				<div class="content">
+            					<h2 class="name">{{food.name}}</h2>
+            					<p class="desc">{{food.description}}</p>
+            					<div class="extra">
+            						<span>月售{{food.sellCount}}份</span>
+            						<span>好评率{{food.rating}}%</span>
+            					</div>
+            					<div class="price">
+            						<span>¥{{food.price}}</span>
+            						<span v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+            					</div>
+            				</div>
+            			</li>
+            		</ul>
+            	</li>
+            </ul>
+		</div>
 	</div>
+
 </template>
 
 <script>
-	 export default{};
-	
+	const ERR_OK=0;
+	export default {
+		props:{
+			seller:{
+				type:Object
+			}
+		},
+		data(){
+			return{
+				goods:[]
+			};
+		},
+		created(){
+			this.classMap = ['decrease','discount','special','invoice','guarantee'];
+			this.$http.get('/api/goods').then(function(response){
+				response=response.body;
+				if(response.errno===ERR_OK){
+					this.goods=response.data;
+					console.log(this.goods);
+				}  
+			});
+			
+		}
+	};
 </script>
-    
+
 <style>
+
+    .goods{
+    	display: flex;
+    	position: absolute;
+    	top: 174px;
+        bottom:46px ;
+    	width: 100%;
+    	overflow: auto;
+    	
+    }
+    
+    .goods>.menu-wrapper{
+    	width: 80px;/*兼容Android浏览器*/
+    	flex: 0 0 80px;
+    	background: #f2f5f7;
+    }
+    .menu-wrapper>ul>.menu-item{
+    	display: table;
+    	height: 54px;
+    	width: 56px;
+    	line-height: 14px;
+    	padding: 0 12px;
+    	/*border: 1px solid red;*/
+    }
+    .menu-item>.text{
+    	display: table-cell;
+    	width: 56px;
+    	vertical-align: middle;
+    	font-size: 12px;
+    	border-bottom: 1px solid rgba(7,17,27,0.1);
+    }
+    .menu-item>.text>.icon {
+		display: inline-block;
+	    vertical-align: top;
+		width: 12px;
+		height: 12px;
+		margin-right: 2px;
+		background-size: 12px 12px;
+		background-repeat: no-repeat;
+	}
+	
+	.icon.decrease {
+		background-image: url(decrease_3@2x.png);
+	}
+	
+	.icon.discount {
+		background-image: url(discount_3@2x.png);
+	}
+	
+	.icon.guarantee {
+		background-image: url(guarantee_3@2x.png);
+	}
+	
+	.icon.invoice {
+		background-image: url(invoice_3@2x.png);
+	}
+	
+	.icon.special {
+		background-image: url(special_3@2x.png);
+	}
+	
+    .goods>.foods-wrapper{
+    	flex: 1;
+    }
+    .foods-wrapper .title{
+    	padding-left: 14px;
+    	height: 26px;
+    	line-height: 26px;
+    	border-left: 2px solid #d9dde1;
+    	font-size: 12px;
+    	color: rgb(147,153,159);
+    	background: #f3f5f7;
+    }
+    .foods-wrapper .food-item{
+    	display: flex;
+    	margin: 18px;
+    	padding-bottom: 18px;
+    	border-bottom: 1px solid rgba(7,17,27,0.1);
+    }
+    .food-item:last-child{
+    	border-bottom: none;
+    	margin-bottom: 0;
+    }
 </style>
